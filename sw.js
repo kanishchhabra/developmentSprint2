e1b44f9ad6c23fb9b5a2915d01670e816079df08
kanishchhabra <s316921@students.cdu.edu.au>
2020-10-16 22:56:48 +0930

    Debugging, please disregard this commit

--- sw.js
+++ sw.js
@@ -5,35 +5,7 @@ var urlsToCache = [
     '/developmentSprint2/',
     '/developmentSprint2/index.html',
 
-    '/developmentSprint2/html/post.html',
-    '/developmentSprint2/html/projects.html',
 
-    '/developmentSprint2/css/style.css',
-
-    '/developmentSprint2/developmentSprint2/js/firebase_projects.js',
-    '/developmentSprint2/js/firebase.js',
-    '/developmentSprint2/js/script_capture.js',
-    '/developmentSprint2/js/script_filter_projects.js',
-    '/developmentSprint2/js/script_filter.js',
-    '/developmentSprint2/js/script_fudge.js',
-    '/developmentSprint2/js/sw_register.js',
-    '/developmentSprint2/js/manifest.json',
-
-    '/developmentSprint2/icons/comments.svg',
-    '/developmentSprint2/icons/cross.svg',
-    '/developmentSprint2/icons/direct.svg',
-    '/developmentSprint2/icons/discover.svg',
-    '/developmentSprint2/icons/filter.svg',
-    '/developmentSprint2/icons/image.svg',
-    '/developmentSprint2/icons/like.svg',
-    '/developmentSprint2/icons/newsfeed.svg',
-    '/developmentSprint2/icons/options.svg',
-    '/developmentSprint2/icons/pin.svg',
-    '/developmentSprint2/icons/plus.svg',
-    '/developmentSprint2/icons/profile.svg',
-    '/developmentSprint2/icons/projects.svg',
-    '/developmentSprint2/icons/star.svg',
-    '/developmentSprint2/icons/stufy.png',
 ];
 
 self.addEventListener('install', function (event) {
